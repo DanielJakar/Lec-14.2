80dc70823f4380c3d81095e2e6865a6349cdb09a
DanielJakar <danieljakar@gmail.com>
2017-02-21 20:47:40 +0200

    Classes in order to build a deck of cards

--- src/course/Daniel/Java/Deck.java
+++ src/course/Daniel/Java/Deck.java
@@ -1,6 +1,7 @@
 package course.Daniel.Java;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Jakars on 21/02/2017.
@@ -10,15 +11,48 @@ public class Deck {
 
     ArrayList<Cards> deck = new ArrayList<>();
 
-    String[] suits = {"♥", "♦", "♣", "♠"};
-    String[] cards = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
 
-    Deck (){
-        for (String card : cards) {
-            for (String suit : suits) {
+    public Deck() {
+        String[] suits = {"♥", "♦", "♣", "♠"};
+        String[] cards = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+
+        for (String suit : suits) {
+            for (String card : cards) {
                 Cards c = new Cards(suit, card);
                 deck.add(c);
             }
         }
+        for (String suit : suits) {
+            for (String card : cards) {
+
+
+            }
+        }
+
+    }
+    public void shuffle(){
+        ArrayList<Cards> temp = new ArrayList<>();
+//        for (int i = deck.size() - 1; i > 0; i--) {
+//            int index = RandomUtils.nextRandom(0,i);
+//            temp.add(deck.remove(index));
+//        }
+        Random r = new Random();
+
+        while (deck.size() > 0){
+            temp.add(deck.remove(r.nextInt(deck.size())))
+        }
+        deck = temp;
+    }
+    public Cards draw (){
+        return deck.remove(0);
+    }
+
+    public boolean isEmpty (){
+        return deck.isEmpty();
+    }
+
+    public ArrayList<Cards> getDeck() {
+        return deck;
     }
 }
